0c9e09cfa91db84253210e509b6ab6618452db12
Martin Fernandez <Electronicsmf@aol.com>
2024-02-06 20:31:24 -0500

    adding es6 code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let tArr = ["Mens", "Womens", "Youth", "Childs"];
-let sArr = ["XL", "M", "S", "XS"];
-let kArr = ["pants", "shirt", "shoes", "socks", "sweater", "belt"];
+const tArr = ["Mens", "Womens", "Youth", "Childs"];
+const sArr = ["XL", "M", "S", "XS"];
+const kArr = ["pants", "shirt", "shoes", "socks", "sweater", "belt"];
 
-function compare(a, b, column, numeric, order) {
+const compare = (a, b, column, numeric, order) => {
   let aValue = a.cells[column].innerHTML;
   let bValue = b.cells[column].innerHTML;
 
@@ -14,27 +14,27 @@ function compare(a, b, column, numeric, order) {
   if (aValue < bValue) return order;
   if (aValue > bValue) return -order;
   return 0;
-}
+};
 
-function sortColumn(header, column) {
+const sortColumn = (header, column) => {
   let rows = document.querySelectorAll("tbody tr");
   rows = Array.from(rows);
 
-  rows.sort(function (a, b) {
-    return compare(a, b, column, header.dataset.numeric, header.dataset.order);
-  });
+  rows.sort((a, b) =>
+    compare(a, b, column, header.dataset.numeric, header.dataset.order)
+  );
 
-  rows.forEach(function (row) {
+  rows.forEach((row) => {
     document.querySelector("tbody").appendChild(row);
   });
 
   header.dataset.order = -header.dataset.order;
 
-  document.querySelectorAll("span").forEach(function (span) {
+  document.querySelectorAll("span").forEach((span) => {
     span.classList.remove("ascending", "descending");
   });
 
-  document.querySelectorAll("td").forEach(function (td) {
+  document.querySelectorAll("td").forEach((td) => {
     td.classList.remove("sortColumn");
   });
 
@@ -44,16 +44,18 @@ function sortColumn(header, column) {
     header.children[0].classList.add("descending");
   }
 
-  document.querySelectorAll("tbody tr td:nth-child(" + (column + 1) + ")").forEach(function (td) {
+  document.querySelectorAll(
+    `tbody tr td:nth-child(${column + 1})`
+  ).forEach((td) => {
     td.classList.add("sortColumn");
   });
-}
+};
 
-function filterColumn(input, column) {
-  document.querySelectorAll("tbody tr").forEach(function (row) {
-    let header = document.querySelector("th:nth-child(" + (column + 1) + ")");
-    let filterVal = input.value;
-    let rowVal = row.cells[column].innerHTML;
+const filterColumn = (input, column) => {
+  document.querySelectorAll("tbody tr").forEach((row) => {
+    const header = document.querySelector(`th:nth-child(${column + 1})`);
+    const filterVal = input.value;
+    const rowVal = row.cells[column].innerHTML;
 
     if (header.dataset.numeric) {
       if (parseFloat(filterVal) >= parseFloat(rowVal)) {
@@ -65,15 +67,13 @@ function filterColumn(input, column) {
       }
     }
   });
-}
+};
 
-function randInt(max) {
-  return Math.floor(Math.random() * max) + 1;
-}
+const randInt = (max) => Math.floor(Math.random() * max) + 1;
 
-function buildData() {
+const buildData = () => {
   for (let x = 1; x < 26; x++) {
-    let row = document.createElement("tr");
+    const row = document.createElement("tr");
     row.appendChild(document.createElement("td")).innerHTML = x;
     row.appendChild(document.createElement("td")).innerHTML =
       tArr[randInt(3)] + " " + sArr[randInt(3)] + " " + kArr[randInt(5)];
@@ -84,30 +84,13 @@ function buildData() {
 
     document.querySelector("tbody").appendChild(row);
   }
-}
+};
 
-document.addEventListener("DOMContentLoaded", function () {
-    buildData();
-  
-    document.querySelectorAll("th").forEach(function (header, i) {
-      let numeric = header.classList.contains("numeric");
-      let order = -1;
-  
-      header.dataset.numeric = numeric;
-      header.dataset.order = order;
-  
-      header.children[0].addEventListener("click", function () {
-        sortColumn(header, i);
-      });
-  
-      var filter = document.createElement("input");
-      filter.type = "text";
-      filter.addEventListener("keyup", function () {
-        filterColumn(filter, i);
-      });
-  
-      header.appendChild(filter);
-    });
-  });
-  
+document.addEventListener("DOMContentLoaded", () => {
+  buildData();
 
+  document.querySelectorAll("th").forEach((header, i) => {
+    const numeric = header.classList.contains("numeric");
+    let order = -1;
+
+    header.da
